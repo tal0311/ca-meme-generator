baf31c3e4a0a61b05561111ac2e853a84d572d75
tal0311 <tal.amit0311@gmail.com>
2022-02-16 10:39:45 +0200

    render img on canvas

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,2 +1,31 @@
 'use strict'
-console.log('controller')
+
+var gElCanvas
+var gCtx
+
+function init() {
+  console.log('init')
+  gElCanvas = document.querySelector('canvas')
+
+  console.log('canvas loaded')
+  gCtx = gElCanvas.getContext('2d')
+  console.log(gElCanvas)
+  // addListeners()
+  renderMeme()
+}
+
+function renderMeme() {
+  console.log('render meme')
+  let meme = getMemeForeDisplay()
+  let { selectedImgId, selectedLineIdx, lines } = meme
+  let img = getImageForDisplay()
+  let { id, url, keywords } = img
+
+  // render img on canvas
+  var renderImg = new Image()
+  renderImg.src = url
+  renderImg.onload = () => {
+    console.log(renderImg)
+    gCtx.drawImage(renderImg, 0, 0, gElCanvas.width, gElCanvas.height)
+  }
+}
--- js/service/meme-servics.js
+++ js/service/meme-servics.js
@@ -1,3 +1,29 @@
 'use strict'
 
-console.log('service');
+var gKeywordSearchCountMap = { funny: 12, cat: 16, baby: 2 }
+var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['trump', 'politics'] }]
+
+var CurrMemeIdx = 0
+
+var gMeme = {
+  selectedImgId: 1,
+  selectedLineIdx: 0,
+  lines: [
+    {
+      txt: 'I sometimes eat Falafel',
+      size: 20,
+      align: 'left',
+      color: 'red',
+    },
+  ],
+}
+
+function getMemeForeDisplay() {
+  console.log('get meme for display')
+  return gMeme
+}
+
+function setCurrMemeIdx() {}
+function getImageForDisplay() {
+  return gImgs[CurrMemeIdx]
+}
